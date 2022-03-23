2c5cb7489208ad4fd40cc4abcd0ca0ed8ec468f6
Daniel Aguiar <daguia@microsoft.com>
2022-03-23 11:42:39 -0500

    Improving Aquino

--- _scripts/aquino-quaresma/splitAquinoQuaresma.py
+++ _scripts/aquino-quaresma/splitAquinoQuaresma.py
@@ -11,7 +11,7 @@ initial_path = "./_scripts/aquino-quaresma/"
 
 
 try:
-    dir = './aquino-quaresma'
+    dir = initial_path + "output/"
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
 except OSError:
@@ -19,17 +19,13 @@ except OSError:
 
 count_file = 0
 
-with open(initial_path + "full.md", encoding="utf-8") as fullFile:
+with open(initial_path + "full - 003 - OL.md", encoding="utf-8") as fullFile:
     lines = fullFile.readlines()
 
-    with open(initial_path + "slugList.txt", encoding="utf-8") as fileNames:
-        fileNamesLines = fileNames.readlines()
-
-        for line in lines:
-            if(line[0:1] == "#"):
-                print(count_file + 1, "- new file - ",
-                      fileNamesLines[count_file].strip())
-                count_file = count_file + 1
-            else:
-                with open("./aquino-quaresma/"+count_file.__str__()+".md", "a", encoding="utf8") as newFile:
-                    newFile.write(line)
+    for line in lines:
+        if(line[0:1] == "#"):
+            print(count_file + 1, "- new file ")
+            count_file = count_file + 1
+        else:
+            with open(initial_path + "output/"+count_file.__str__()+".md", "a", encoding="utf8") as newFile:
+                newFile.write(line)
